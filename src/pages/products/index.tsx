ad24f6c6f11226265fded4515386022f9d125b0e
yeonhwa <yeonhwa6717@gmail.com>
2023-07-31 23:57:42 +0900

    Static Routing (in pages).

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProductsPage() {
+  return (
+    <h1>제품 소개 페이지!</h1>
+  );
+}
+
+export default ProductsPage;
---
+++ src/pages/products/pants.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PantsPage() {
+  return (
+    <h1>바지 제품 설명 페이지!</h1>
+  );
+}
+
+export default PantsPage;
