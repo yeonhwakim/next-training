227ea71456645ff174034fe3db159eb086e9ffe4
yeonhwa <yeonhwa6717@gmail.com>
2023-08-04 22:10:41 +0900

    Add dynamic metadata.

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -2,24 +2,29 @@ import { notFound } from "next/navigation";
 
 type Props = {
   params: {
-    slug: string
-  }
+    slug: string;
+  };
+};
+
+export function generateMetadata({ params: { slug } }: Props) {
+  return {
+    title: `${slug} 제품`,
+    description: `${slug} 제품 상세 페이지입니다.`,
+  };
 }
 
-function SlugPage({params}: Props) {
-  if(params.slug === 'nothing') {
-    return notFound()
+function SlugPage({ params }: Props) {
+  if (params.slug === "nothing") {
+    return notFound();
   }
-  
-  return (
-    <h1>{params.slug} 제품 설명 페이지!</h1>
-  );
+
+  return <h1>{params.slug} 제품 설명 페이지!</h1>;
 }
 
 export default SlugPage;
 
 export function generateStaticParams() {
-  const products = ['pants', 'skirts']
+  const products = ["pants", "skirts"];
 
-  return products.map(product =>({slug: product}))
+  return products.map((product) => ({ slug: product }));
 }
