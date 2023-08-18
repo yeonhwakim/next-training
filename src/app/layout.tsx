d5df8e1b2161d1e2926a671528326cc836747541
yeonhwa <yeonhwa6717@gmail.com>
2023-08-18 22:39:41 +0900

    Implement font.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,22 @@ import Link from "next/link";
 import "./globals.css";
 import styles from "./layout.module.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto, Open_Sans, Nanum_Gothic } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+const sans = Open_Sans({
+  weight: "500",
+  subsets: ["latin"],
+  display: "swap",
+});
+const namum = Nanum_Gothic({
+  subsets: ["latin"],
+  weight: "700",
+});
 
 export const metadata: Metadata = {
   title: "Next Training",
@@ -17,10 +30,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body>
-        <header className={styles.header}>
-          <h1>Demo Note</h1>
+        <header className={[styles.header, sans.className].join(" ")}>
+          <h1 className={namum.className}>Demo Note</h1>
           <nav className={styles.nav}>
             <Link href="/products">Products</Link>
             <Link href="/about">About</Link>
