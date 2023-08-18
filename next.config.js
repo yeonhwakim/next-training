5ccac6833acbc15f61d8f08abb7767640d12751c
yeonhwa <yeonhwa6717@gmail.com>
2023-08-18 22:49:20 +0900

    Redirect setting in next.config.js.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/products/deleted_forever",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/products/deleted_temp",
+        destination: "/products",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
